ab6819ec436f0e0182bdf4766610a9802b409279
jaimezzapata <zapataval2304@gmail.com>
2023-09-27 20:50:27 -0500

    Se finaliza la función de listar elementos de firebase

--- src/components/layouts/ListadoEmpresas.jsx
+++ src/components/layouts/ListadoEmpresas.jsx
@@ -1,11 +1,34 @@
+import { connDataBase } from "../database/config-firebase";
+import {collection, getDocs} from 'firebase/firestore'
 import Header from "../helpers/Header";
+import { useState, useEffect } from "react";
 
 const ListadoEmpresas = () => {
+  const [empresas, setEmpresas] = useState([])
+
+  const getEmpresas = async () => {
+    const empresasCollection = collection(connDataBase, 'empresas')
+    let data = await getDocs(empresasCollection)
+    console.log(data.docs.map((doc)=>(doc.data())))
+    setEmpresas(data.docs.map((doc)=>(doc.data())))
+  }
+  useEffect(()=>{
+    getEmpresas()
+  },[])  
+
   return (
     <section>
       <Header />
       <section>
-        <h1>Listado de empresas</h1>
+        {
+          empresas.map((empresa)=>(
+            <section>
+              <p>Nombre empresa: {empresa.nombreEmpresa}</p>
+              <p>Direccion empresa: {empresa.direccionEmpresa}</p>
+              <p>Telefono gerente: {empresa.telefonoGerente}</p>
+            </section>
+          ))
+        }
       </section>
     </section>
   );
